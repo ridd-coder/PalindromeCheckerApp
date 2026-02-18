10983849a01c273d451d1b4e4d7adb0de4711431
ridd-coder <riddhimandutta0@gmail.com>
2026-02-18 15:33:19 +0530

    Added Use Case 3 - Reverse String Palindrome
    # Conflicts:
    #	PalindromeCheckerApp/src/UC3PalindromeCheckUsingStringReverse.java

---
+++ PalindromeCheckerApp/src/UC3PalindromeCheckUsingStringReverse.java
@@ -0,0 +1,23 @@
+public class UC3PalindromeCheckUsingStringReverse {
+
+    /**
+     * Application entry point for UC3.
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        String input = "madam";
+        String reversed = "";
+
+        // Iterate from last character to first
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        boolean isPalindrome = input.equals(reversed);
+
+        System.out.println("Input text: " + input);
+        System.out.println("Reversed text: " + reversed);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
+}
