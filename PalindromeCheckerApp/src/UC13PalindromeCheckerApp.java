6c45f6e373578b1f166fbae0344e12ad2148ab78
Riddhiman Dutta <riddhimandutta@Riddhimans-MacBook-Air-3.local>
2026-03-05 23:08:51 +0530

    Added UC13 Performance Comparison for Palindrome Algorithms

---
+++ PalindromeCheckerApp/src/UC13PalindromeCheckerApp.java
@@ -0,0 +1,35 @@
+public class UC13PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        long startTime = System.nanoTime();
+
+        boolean isPalindrome = check(input);
+
+        long endTime = System.nanoTime();
+
+        long executionTime = endTime - startTime;
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + executionTime + " ns");
+    }
+
+    public static boolean check(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
